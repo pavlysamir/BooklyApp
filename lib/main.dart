2cfd099709da69273f63fcce8e36a61289561e69
pavlysamir <bavlysamir33@gmail.com>
2023-06-27 09:32:17 +0200

    open box books

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+void main() async {
   runApp(const BooklyApp());
+
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBooks);
 }
 
 class BooklyApp extends StatelessWidget {
