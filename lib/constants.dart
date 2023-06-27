5e569bec66efb30b1c61dff36844bd0dc5cc9d6d
pavlysamir <bavlysamir33@gmail.com>
2023-06-27 09:31:55 +0200

    create box featured books

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,6 @@
-
-
 import 'dart:ui';
 
 const kPrimaryKey = Color(0xff100B20);
 const kDuration = Duration(milliseconds: 250);
 const kGtsectraFine = 'GT Sectra Fine';
+const kFeaturedBooks = 'Featured Books';
