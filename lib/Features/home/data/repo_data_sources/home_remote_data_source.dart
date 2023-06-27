cde217c6597de5270b77d4537dfaf5b038931362
pavlysamir <bavlysamir33@gmail.com>
2023-06-27 07:44:30 +0200

    implement fetch featured news books

--- lib/Features/home/data/repo_data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/repo_data_sources/home_remote_data_source.dart
@@ -16,30 +16,21 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
     return books;
   }
 
+  @override
+  Future<List<BookEntity>> fetchNewsBooks() async {
+    var data = await apiServices.get(
+        endPoint:
+            'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
+    List<BookEntity> newsBooks = getBooksList(data);
+    return newsBooks;
+  }
+
   List<BookEntity> getBooksList(Map<String, dynamic> data) {
     List<BookEntity> books = [];
     for (var bookMap in data['items']) {
       books.add(BookModel.fromJson(bookMap));
     }
     return books;
-  }
-
-  @override
-  Future<List<BookEntity>> fetchNewsBooks() async {
-    var data = await apiServices.get(
-        endPoint:
-            'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
-    List<BookEntity> newsBooks = getNewsBooksList(data);
-    return newsBooks;
-  }
-
-  List<BookEntity> getNewsBooksList(Map<String, dynamic> data) {
-    List<BookEntity> newsBooks = [];
-
-    for (var booksMap in data['items']) {
-      newsBooks.add(BookModel.fromJson(booksMap));
-    }
-    return newsBooks;
   }
 }
 
